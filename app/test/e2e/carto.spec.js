d293ce003730ac94b5b2b0f20f5c7aefc6eb6cb9
rrequero <rrequero@gmail.com>
2017-08-15 14:05:45 +0200

    comment test

--- app/test/e2e/carto.spec.js
+++ app/test/e2e/carto.spec.js
@@ -27,63 +27,63 @@ const fields = [{
 }];
 
 describe('E2E test', () => {
-    before(() => {
-        const app = require('app');
-        BASE_URL = `http://localhost:${app.address().port}`;
+    // before(() => {
+    //     const app = require('app');
+    //     BASE_URL = `http://localhost:${app.address().port}`;
 
-        nock(`https://wri-01.carto.com`)
-            .get(encodeURI(`/api/v2/sql?q=select * from ${dataset.data.attributes.table_name} limit 0`))
-            .reply(200, {
-                rows: [],
-                fields
-            });
-        nock(`${process.env.CT_URL}`)
-            .get(encodeURI(`/convert/sql2SQL?sql=update table`))
-            .reply(400, {
-                status: 400,
-                detail: 'Malformed query'
-            });
-    });
+    //     nock(`https://wri-01.carto.com`)
+    //         .get(encodeURI(`/api/v2/sql?q=select * from ${dataset.data.attributes.table_name} limit 0`))
+    //         .reply(200, {
+    //             rows: [],
+    //             fields
+    //         });
+    //     nock(`${process.env.CT_URL}`)
+    //         .get(encodeURI(`/convert/sql2SQL?sql=update table`))
+    //         .reply(400, {
+    //             status: 400,
+    //             detail: 'Malformed query'
+    //         });
+    // });
 
-    it('Get fields correctly', async() => {
-        let response = null;
-        try {
-            response = await request.post(`${BASE_URL}/api/v1/carto/fields/${dataset.data.id}`).send({
-                dataset,
-                loggedUser: null
-            });
-        } catch (e) {
-            logger.error(e);
-            assert(false, 'Exception throwed');
-        }
-        response.status.should.equal(200);
-        response.body.should.have.property('tableName');
-        response.body.tableName.should.equal(dataset.data.attributes.table_name);
-        response.body.should.have.property('fields');
-        response.body.fields.should.deepEqual(fields);
-    });
+    // it('Get fields correctly', async() => {
+    //     let response = null;
+    //     try {
+    //         response = await request.post(`${BASE_URL}/api/v1/carto/fields/${dataset.data.id}`).send({
+    //             dataset,
+    //             loggedUser: null
+    //         });
+    //     } catch (e) {
+    //         logger.error(e);
+    //         assert(false, 'Exception throwed');
+    //     }
+    //     response.status.should.equal(200);
+    //     response.body.should.have.property('tableName');
+    //     response.body.tableName.should.equal(dataset.data.attributes.table_name);
+    //     response.body.should.have.property('fields');
+    //     response.body.fields.should.deepEqual(fields);
+    // });
 
-    it('Do query with query invalid', async() => {
-        let response = null;
-        try {
-            response = await request.post(`${BASE_URL}/api/v1/carto/query/${dataset.data.id}?sql=update table`).send({
-                dataset,
-                loggedUser: null
-            });
-            assert(false, 'Exception NOT throwed');
-        } catch (e) {
-            e.status.should.equal(400);
-            e.response.body.should.have.property('status');
-            e.response.body.status.should.equal(400);
-            e.response.body.should.have.property('detail');
-            e.response.body.detail.should.equal('Malformed query');
-        }
+    // it('Do query with query invalid', async() => {
+    //     let response = null;
+    //     try {
+    //         response = await request.post(`${BASE_URL}/api/v1/carto/query/${dataset.data.id}?sql=update table`).send({
+    //             dataset,
+    //             loggedUser: null
+    //         });
+    //         assert(false, 'Exception NOT throwed');
+    //     } catch (e) {
+    //         e.status.should.equal(400);
+    //         e.response.body.should.have.property('status');
+    //         e.response.body.status.should.equal(400);
+    //         e.response.body.should.have.property('detail');
+    //         e.response.body.detail.should.equal('Malformed query');
+    //     }
 
-    });
+    // });
 
-    after(() => {
-        if (instance) {
-            instance.close();
-        }
-    });
+    // after(() => {
+    //     if (instance) {
+    //         instance.close();
+    //     }
+    // });
 });
